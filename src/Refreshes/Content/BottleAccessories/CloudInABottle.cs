c2428b7af3f33cd1181624a71eaeab7725ef7260
mayli4 <sydsleeper@gmail.com>
2025-12-19 17:19:52 -0500

    wip: particles i didnt commit

--- src/Refreshes/Content/BottleAccessories/CloudInABottle.cs
+++ src/Refreshes/Content/BottleAccessories/CloudInABottle.cs
@@ -1,5 +1,8 @@
 ﻿using Daybreak.Common.Features.Hooks;
 using MonoMod.RuntimeDetour;
+using Refreshes.Common.Particles;
+using Terraria.Audio;
+using Terraria.Graphics.Renderers;
 
 namespace Refreshes.Content.BottleAccessories;
 
@@ -8,13 +11,71 @@ internal sealed class CloudInABottleModifications {
     [ModPlayerHooks.CanShowExtraJumpVisuals]
     public static bool CancelVanillaVisuals(ExtraJump jump) => jump != ExtraJump.CloudInABottle;
     
-    private static Hook _hook = null!;
+    private static Hook hook = null!;
     private delegate void orig_OnStarted(CloudInABottleJump self, Player player, ref bool playSound);
-
-    //cancels out initial cloud puff
+    
+    //cancels out initial cloud burst, replaces with our own
     [OnLoad]
-    private static void HookCloud() => _hook = new Hook(typeof(CloudInABottleJump).GetMethod("OnStarted")!, (orig_OnStarted _, CloudInABottleJump _, Player _, ref bool _) => { });
+    private static void HookCloud() {
+        hook = new Hook(
+            typeof(CloudInABottleJump).GetMethod("OnStarted")!, 
+            (orig_OnStarted _, CloudInABottleJump _, Player player, ref bool playSound) => {
+                playSound = false;
+
+                SoundEngine.PlaySound(Assets.Sounds.Items.CloudJump.Asset with { PitchVariance = 0.3f, Pitch = 0f});
+                for (int i = 0; i < 15; i++) {
+                    var p = CloudJumpParticle.Pool.RequestParticle(); 
+                    
+                    p.Position = player.Bottom;
+                    p.Velocity = Main.rand.NextVector2Circular(4f, 2f); 
+                    p.Velocity.Y -= player.velocity.Y * 0.8f;
+                    p.Velocity.X -= player.velocity.X * 0.4f;
+                    
+                    p.Scale = Main.rand.NextFloat(2f, 0.5f);
+                    p.Rotation = Main.rand.NextFloat(MathHelper.TwoPi);
+                    p.RotationSpeed = Main.rand.NextFloat(-0.1f, 0.1f);
+                
+                    ParticleEngine.Particles.Add(p);
+                }
+            }
+        );
+    }
     
     [OnUnload]
-    private static void Unhook() => _hook.Dispose();
+    private static void Unhook() => hook.Dispose();
+}
+
+
+[PoolCapacity(300)]
+public sealed class CloudJumpParticle : BaseParticle<CloudJumpParticle> {
+    public Vector2 Position;
+    public Vector2 Velocity;
+    public float Scale;
+    public float Rotation;
+    public float Alpha;
+    public float RotationSpeed;
+
+    public override void FetchFromPool() {
+        base.FetchFromPool();
+        Alpha = 1f;
+    }
+
+    public override void Update(ref ParticleRendererSettings settings) {
+        Position += Velocity;
+        Velocity *= 0.94f;
+        Rotation += RotationSpeed;
+        Scale *= 0.97f;
+        Alpha -= 0.03f;
+
+        if (Alpha <= 0 || Scale <= 0.1f)
+            ShouldBeRemovedFromRenderer = true;
+    }
+
+    public override void Draw(ref ParticleRendererSettings settings, SpriteBatch spriteBatch) {
+        var tex = Assets.Images.UI.AuthorTags.Tobias.Asset.Value; 
+        var origin = tex.Size() / 2;
+        var color = Color.White * Alpha;
+
+        spriteBatch.Draw(tex, Position + settings.AnchorPosition, null, color, Rotation, origin, Scale, SpriteEffects.None, 0f);
+    }
 }
