53feb9fa00fdade975624c3c907485826057c31a
Roton <140523534+RotonDev@users.noreply.github.com>
2025-12-24 14:27:15 +0100

    feat: boots spawn tile specific dusts when running

---
+++ src/Refreshes/Content/RunningEffects/RunDustSwap.cs
@@ -0,0 +1,50 @@
+﻿using Daybreak.Common.Features.Hooks;
+using MonoMod.RuntimeDetour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using MonoMod.Cil;
+using Terraria.ID;
+using Terraria.Graphics.Shaders;
+
+namespace Refreshes.Content.RunningEffects;
+
+internal sealed class RunDustSwap {
+    private static ILHook _spawnFastRunParticlesHook;
+    [OnLoad]
+    private static void ApplyEdits() {
+        var m_spawnFastRunParticles = typeof(Player).GetMethod("SpawnFastRunParticles", BindingFlags.Instance | BindingFlags.NonPublic);
+        _spawnFastRunParticlesHook = new ILHook(m_spawnFastRunParticles, ReplaceRunningDusts);
+    }
+    [OnUnload]
+    private static void Unhook() => _spawnFastRunParticlesHook.Dispose();
+    private static void ReplaceRunningDusts(ILContext ctx) {
+        ILCursor c = new ILCursor(ctx);
+        while (c.TryGotoNext(MoveType.Before, i => i.MatchCall("Terraria.Dust", "NewDust")))
+            continue;
+        // remove original NewDust call due to a different approach
+        c.Remove();
+        // push player onto stack
+        c.EmitLdarg0();
+        // capture original call parameters to not mess with compat
+        c.EmitDelegate((Vector2 position, int width, int height, int type, float speedX, float speedY, int alpha, Color color, float scale, Player player) =>
+        {
+            if (Main.rand.NextBool(3))
+            {
+                Point tilePos = position.ToTileCoordinates();
+                int dustWhoAmI = WorldGen.KillTile_MakeTileDust(tilePos.X, tilePos.Y, Main.tile[tilePos]);
+                Main.dust[dustWhoAmI].position.X = player.Center.X;
+                // move up so the dust isnt at the center of the tile below the player
+                Main.dust[dustWhoAmI].position.Y -= 8;
+                Main.dust[dustWhoAmI].velocity.Y = -2f;
+                Main.dust[dustWhoAmI].velocity.X = player.direction * 2;
+                Main.dust[dustWhoAmI].scale *= 0.8f;
+                Main.dust[dustWhoAmI].shader = GameShaders.Armor.GetSecondaryShader(player.cShoe, player);
+            }
+        });
+        c.EmitRet();
+    }
+}
