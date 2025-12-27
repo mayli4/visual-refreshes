c25516188d1823820f61b5b0e9b38edcd3517ad6
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-12-27 19:44:13 +0200

    feat: wispier + colored trail; disabled dust experiment

---
+++ src/Refreshes/Content/Dusts/LightDotRGB.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+
+namespace Refreshes.Content.Dusts;
+
+internal class LightDotRGB : ModDust
+{
+    public override string? Texture => null;
+
+    public override void OnSpawn(Dust dust)
+    {
+        var desiredVanillaDustTexture = DustID.FireworksRGB;
+        var frameX = desiredVanillaDustTexture * 10 % 1000;
+        var frameY = desiredVanillaDustTexture * 10 / 1000 * 30 + Main.rand.Next(3) * 10;
+        dust.frame = new Rectangle(frameX, frameY, 8, 8);
+    }
+
+    public override bool Update(Dust dust)
+    {
+        dust.scale -= dust.fadeIn;
+
+        if (!dust.noLight && !dust.noLightEmittence)
+        {
+            Lighting.AddLight(dust.position, dust.color.ToVector3() * dust.scale);
+        }
+        if (dust.noGravity)
+        {
+            dust.velocity *= 0.93f;
+        }
+        else
+        {
+            dust.velocity *= 0.95f;
+            dust.scale -= 0.0025f;
+        }
+
+        return true;
+    }
+
+    public override Color? GetAlpha(Dust dust, Color lightColor)
+    {
+        var result = new Color(lightColor.ToVector3() * dust.color.ToVector3())
+        {
+            A = 25
+        };
+        return result;
+    }
+}
--- src/Refreshes/Content/Yoyos/Terrarian.cs
+++ src/Refreshes/Content/Yoyos/Terrarian.cs
@@ -1,9 +1,13 @@
 ﻿using Daybreak.Common.Features.Hooks;
+using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
 using Microsoft.Xna.Framework.Graphics;
+using Refreshes.Common.Particles;
 using Refreshes.Common.Rendering;
+using Refreshes.Content.Dusts;
 using Refreshes.Core;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using Terraria.GameContent;
 using Terraria.ID;
@@ -12,8 +16,14 @@ namespace Refreshes.Content.Yoyos;
 
 internal sealed class TerrarianModifications : GlobalProjectile
 {
-    private static readonly Color trail_color = new Color(0.4f, 1f, 0.6f) * 0.5f;
+    private static readonly Color trail_color_start = new Color(0.4f, 1f, 0.6f) * 0.5f;
+    private static readonly Color trail_color_end = new Color(0.4f, 0.6f, 1f) * 0.5f;
+
     private float TrailWidth(float p) => float.Lerp(6f, 12f, p);
+
+    private const float trail_offset_amplitude = 10f;
+    private const float trail_offset_period = 0.2f;
+    private const float trail_offset_phase = 1f;
 
     private const float trail_split_width = 0.65f;
     private const float trail_split_start = 50f;
@@ -22,7 +32,11 @@ internal sealed class TerrarianModifications : GlobalProjectile
     private const float trail_fadeoff_length = 120f;
 
     private Vector2[]? previousPositions;
+    private Vector2[]? previousDirections;
+    private float[]? previousOffsetPhases;
+    private Vector2[]? previousOffsetPositions;
     private float[]? previousRotations;
+    private float currentPhase;
     private float totalDistance;
 
     public override bool InstancePerEntity => true;
@@ -34,37 +48,86 @@ internal sealed class TerrarianModifications : GlobalProjectile
 
     public override void PostAI(Projectile projectile)
     {
-        if (previousPositions == null || previousRotations == null)
+        if (previousPositions == null || previousDirections == null || previousOffsetPhases == null || previousOffsetPositions == null || previousRotations == null)
         {
             previousPositions = new Vector2[20];
+            previousDirections = new Vector2[20];
+            previousOffsetPhases = new float[20];
+            previousOffsetPositions = new Vector2[20];
             previousRotations = new float[20];
 
             for (var i = 0; i < previousPositions.Length; i++)
             {
                 previousPositions[i] = projectile.position + projectile.velocity;
-                previousRotations[i] = projectile.velocity.ToRotation();
+                previousDirections[i] = projectile.velocity.SafeNormalize(Vector2.Zero);
+                previousOffsetPhases[i] = 0;
+                previousOffsetPositions[i] = previousPositions[i];
+                previousRotations[i] = previousDirections[i].ToRotation();
             }
         }
 
         for (var i = previousPositions.Length - 1; i > 0; i--)
         {
+            var progress = (float)i / previousOffsetPositions.Length;
+            var offset = projectile.velocity.RotatedBy(MathF.PI / 2) * MathF.Sin(Main.GlobalTimeWrappedHourly + i * trail_offset_period) * 5f * progress;
+
             previousPositions[i] = previousPositions[i - 1];
+            previousDirections[i] = previousDirections[i - 1];
+            previousOffsetPhases[i] = previousOffsetPhases[i - 1];
             previousRotations[i] = previousRotations[i - 1];
         }
+
+        currentPhase += trail_offset_phase;
         previousPositions[0] = projectile.position + projectile.velocity;
-        previousRotations[0] = projectile.velocity.ToRotation();
+        previousDirections[0] = projectile.velocity.SafeNormalize(Vector2.Zero);
+        previousOffsetPhases[0] = currentPhase;
+        previousRotations[0] = previousDirections[0].ToRotation();
+
+        for (var i = 0; i < previousOffsetPositions.Length; i++)
+        {
+            var progress = (float)i / previousOffsetPositions.Length;
+            var offset = previousDirections[i].RotatedBy(MathF.PI / 2) * MathF.Sin(previousOffsetPhases[i]) * trail_offset_amplitude * progress;
+            previousOffsetPositions[i] = previousPositions[i] + offset;
+        }
 
         totalDistance = 0;
         for (var i = 0; i < previousPositions.Length - 2; i++)
         {
             totalDistance += (previousPositions[i + 1] - previousPositions[i]).Length();
         }
+
+        /*
+        if (Main.rand.NextBool(3))
+        {
+            var dust = Dust.NewDustPerfect
+            (
+                Main.rand.NextFromList(previousPositions) + projectile.Size * 0.5f + Main.rand.NextVector2Circular(1f, 1f), 
+                ModContent.DustType<LightDotRGB>(),
+                Main.rand.NextVector2Circular(1f, 1f),
+                Scale: 1.5f,
+                newColor: Color.Lerp(Color.White, Color.SpringGreen, Main.rand.NextFloat())
+            );
+            dust.fadeIn = Main.rand.NextFloat(0.1f, 0.3f);
+            dust.noGravity = true;
+        }
+        */
     }
 
     public override bool PreDraw(Projectile projectile, ref Color lightColor)
     {
-        Debug.Assert(previousPositions != null);
+        Debug.Assert(previousOffsetPositions != null);
         Debug.Assert(previousRotations != null);
+
+        /*previousOffsetPositions ??= new Vector2[20];
+        previousOffsetRotations ??= new float[20];
+
+        for (var i = 0; i < previousOffsetPositions.Length; i++)
+        {
+            var progress = (float)i / previousOffsetPositions.Length;
+            var offset = projectile.velocity.RotatedBy(MathF.PI / 2) * MathF.Sin(Main.GlobalTimeWrappedHourly + i * trail_offset_period) * 5f * progress;
+            previousOffsetPositions[i] = previousPositions[i] + offset;
+            previousOffsetRotations[i] = previousDirections[i].SafeNormalize(Vector2.UnitX).ToRotation();
+        }*/
 
         var yoyoTexture = TextureAssets.Projectile[ProjectileID.Terrarian].Value;
 
@@ -87,10 +150,10 @@ internal sealed class TerrarianModifications : GlobalProjectile
         Color StripColorFunction(float p)
         {
             var trailFadeoffProgress = Utils.GetLerpValue(trailFadeoffLengthNormalized, 1f, p, true);
-            return Color.Lerp(trail_color, Color.Transparent, trailFadeoffProgress);
+            return Color.Lerp(Color.Lerp(trail_color_start, trail_color_end, trailFadeoffProgress), Color.Transparent, trailFadeoffProgress);
         }
 
-        StripRenderer.DrawStripPadded(previousPositions, previousRotations, StripColorFunction, TrailWidth, positionOffset - Main.screenPosition, false);
+        StripRenderer.DrawStripPadded(previousOffsetPositions, previousRotations, StripColorFunction, TrailWidth, positionOffset - Main.screenPosition, false);
 
         Main.pixelShader.CurrentTechnique.Passes[0].Apply();
 
