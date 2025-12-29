2e131447d106385a1a8f82f24e4873164bd660bf
mayli4 <sydsleeper@gmail.com>
2025-12-29 05:35:48 -0500

    wip: sandstorm bottle stubs

---
+++ src/Refreshes/Content/BottleAccessories/SandstormInABottle.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using Daybreak.Common.Features.Hooks;
+
+namespace Refreshes.Content;
+
+[Autoload(Side = ModSide.Client)]
+internal static class SandstormInABottle
+{
+    [OnLoad]
+    private static void ApplyHooks()
+    {
+        MonoModHooks.Add(
+            typeof(SandstormInABottleJump).GetMethod("OnStarted", BindingFlags.Public | BindingFlags.Instance)!,
+            OnStarted_SpawnOurSandstormParticles
+        );
+    }
+
+    private static void OnStarted_SpawnOurSandstormParticles(SandstormInABottleJump _, Player player, ref bool playSound)
+    {
+        
+    }
+    
+    [ModPlayerHooks.CanShowExtraJumpVisuals]
+    public static bool CancelVanillaVisuals(ExtraJump jump)
+    {
+        return jump != ExtraJump.SandstormInABottle;
+    }
+}
